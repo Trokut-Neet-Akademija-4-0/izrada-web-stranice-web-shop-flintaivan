d48313c336c1839c0ec147d43213d7431fb7ae42
flintaivan <flinta.ivan@gmail.com>
2024-01-10 13:34:13 +0100

    Refactor ProductItem to component, add style

---
+++ src/components/ProductItem.vue
@@ -0,0 +1,43 @@
+<template>
+    <div>
+        <div class="product-item" :key="props.product.id">
+            <img :src="props.product.thumbnail" alt="" width="300" height="170">
+            <div class="product-item-content">
+                <div class="product-item-info">
+                    <h2>{{ props.product.title }}</h2>
+                    <p>${{ props.product.price }}</p>
+                </div>
+                <div class="product-item-desc">
+                    <p>{{ props.product.description }}</p>
+                </div>
+                <div class="product-item-links">
+                    <router-link :to="{name: 'ProductDetails', params: {id: props.product.id}}" class="btn btn-light-outline">Show more</router-link>
+                    <button @click.stop="addToCart(props.product.id)" class="btn btn-primary-outline">Add to cart</button>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script setup>
+import { defineProps } from 'vue';
+import { productsStore } from '@/stores/products';
+
+const props = defineProps({
+    product: Object
+})
+const products = productsStore();
+const addToCart = (id) => {
+    products.addToCart(id)
+}
+</script>
+
+<style scoped>
+h2 {
+    margin: 12px 0;
+}
+button {
+    margin-left: 10px;
+}
+
+</style>
--- src/views/products/Products.vue
+++ src/views/products/Products.vue
@@ -2,24 +2,13 @@
   <div class="content-wrapper">
       <h1>Products</h1>
       <div class="products-list">
-        <div class="product-item" v-for="product in products.products" :key="product.id" @click="goToProductPage(product.id)">
-            <img :src="product.thumbnail" alt="" width="300" height="170">
-            <h2>{{ product.title }}</h2>
-            <p>{{ product.description }}</p>
-            <p>${{ product.price }}</p>
-        </div>
+        <ProductItem v-for="item in products.products" :key="item.id" :product="item" @click="goToProductPage(item.id)" />
       </div>
     </div>
 </template>
 
-<!-- <script>
-import { defineComponent } from 'vue';
-export default defineComponent({
-  name: 'Products',
-})
-</script> -->
-
 <script setup>
+import ProductItem from '@/components/ProductItem.vue';
 import { onMounted } from 'vue';
 import { productsStore } from '@/stores/products';
 import { useRouter } from 'vue-router';
@@ -37,17 +26,5 @@ onMounted(() => {
 </script>
 
 <style scoped>
-.products-list {
-  display: flex;
-  flex-wrap: wrap;
-  justify-content: space-between;
-  cursor: pointer;
-}
-.product-item {
-  width: 30%;
-  margin-bottom: 20px;
-}
-.product-item img {
-  width: 100%;
-}
+
 </style>
