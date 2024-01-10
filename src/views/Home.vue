3b724f857567e1b45ca539f4a7908f056cda197e
flintaivan <flinta.ivan@gmail.com>
2024-01-10 11:59:36 +0100

    Create and display header

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,5 +1,10 @@
 <template>
+    <Header />
     <div>
-        <h1>This is homepage</h1>
+        <!-- <h1>This is homepage</h1> -->
     </div>
 </template>
+
+<script setup>
+import Header from '../components/Header.vue';
+</script>
